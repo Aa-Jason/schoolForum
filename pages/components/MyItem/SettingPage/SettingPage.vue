352b38b110d2321eb9d7146bff22e95c42433c3c
xqp <1511311145@qq.com>
2022-07-25 18:22:34 +0800

    sss

---
+++ pages/components/MyItem/SettingPage/SettingPage.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="u-page">
+		<view class="u-demo-block">
+			<text class="u-demo-block__title">是否开启评论推送</text>
+			<view class="u-demo-block__content">
+				<view class="u-page__tag-item">
+					<u-switch
+						v-model="value1"
+						@change="change"
+					></u-switch>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				value1: false,
+			};
+		},
+		methods: {
+			change(e) {
+				console.log('change', e);
+			},
+			onLoad: function(option) { //option为object类型，会序列化上个页面传递的参数
+			console.log(option);
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+		.u-page {
+			&__tag-item {
+				margin-right: 30px;
+			}
+		}
+		.u-demo-block__content {
+			flex-direction: row;
+			flex-wrap: wrap;
+			align-items: center;
+		}
+</style>
---
+++ pages/components/MyItem/identifyPage/identifyPage.vue
@@ -0,0 +1,42 @@
+<template>
+	 <view class="u-page__item">
+	       <u-collapse
+	           @change="change"
+	           @close="close"
+	           @open="open"
+	         >
+	           <u-collapse-item
+	             title="姓名"
+	             name="name"
+	           >
+	             <text class="u-collapse-content">涵盖uniapp各个方面，给开发者方向指导和设计理念，让您茅塞顿开，一马平川</text>
+	           </u-collapse-item>
+	           <u-collapse-item
+	             title="学号"
+	             name="id"
+	           >
+	             <text class="u-collapse-content">众多组件覆盖开发过程的各个需求，组件功能丰富，多端兼容。让您快速集成，开箱即用</text>
+	           </u-collapse-item>
+	           <u-collapse-item
+	             title="学院"
+	             name="institute"
+	           >
+	             <text class="u-collapse-content">众多的贴心小工具，是您开发过程中召之即来的利器，让您飞镖在手，百步穿杨</text>
+	           </u-collapse-item>
+	         </u-collapse>
+	    </view>
+</template>
+	
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
