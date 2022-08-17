d4228d6434e69dcba75369eebe33f8b546ad2fb7
1548798163 <1548798163@qq.com>
2022-08-17 17:34:47 +0800

    图标修改

--- pages/components/Info/Info.vue
+++ pages/components/Info/Info.vue
@@ -3,8 +3,8 @@
 		<view class="">
 			<view class="cellsetting">
 			<u-cell-group>
-				<u-cell icon="integral-fill" title="点赞" @click="toSupportPage" size="large" isLink url=""></u-cell>
-				<u-cell icon="integral-fill" title="评论&回复" @click="toAnswerPage" value="" size="large" isLink url=""></u-cell>
+				<u-cell icon="/static/info/点赞.png" title="点赞" @click="toSupportPage" size="large" isLink url=""></u-cell>
+				<u-cell icon="/static/info/评论.png" title="评论&回复" @click="toAnswerPage" value="" size="large" isLink url=""></u-cell>
 			</u-cell-group>
 			</view>
 		</view>
